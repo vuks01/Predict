4e7c62a2b3f88ffa01d86949b55e200e633980eb
Refiloe <phipaelvis3@gmail.com>
2020-02-14 12:05:15 +0200

    Added Function5

---
+++ Function5.py
@@ -0,0 +1,21 @@
+### Starting a function
+def number_of_tweets_per_day(df):
+    # Making a copy of the DataFrame
+    df1 = df.copy()
+    # Extracting a column of dates and turning it into a list
+    dates = list(df1['Date'])
+    # Intializing an empty list to store dates without time
+    date_only = []
+    # Iterating over dates to get dates only
+    for date in dates:
+        date_only.append(date[:10])
+    # Creating an empty list
+    data = pd.DataFrame()
+    #inputing data into the empty DataFrame and creating columns
+    data['Date'] = date_only
+    data['Tweets'] = 1
+    #Grouping by date and getting the some of tweets per day
+    data = data.groupby(['Date']).sum()
+    return data
+
+
