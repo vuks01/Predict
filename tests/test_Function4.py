86bbbd0f9623691aa9f90a7318cd582e7e65635c
phipaelvis <60384369+phipaelvis@users.noreply.github.com>
2020-02-18 15:03:17 +0200

    Add files via upload
    Test for function 4

---
+++ tests/test_Function4.py
@@ -0,0 +1,10 @@
+from Functions import Function4
+
+def test_Function4():
+
+    """use this to check if Function4 is working fine"""
+
+    assert Function4.extract_municipality_hashtags(twitter_df.copy()).iloc[[0]] == 0 Tweets: @BongaDlulane Please send an email to mediades... Date: 2019-11-29 12:50:54 municipality: NaN hashtags: NaN , 'incorrect'
+    assert Function4.extract_municipality_hashtags(twitter_df.copy()).iloc[[1]] == 1 Tweets: @saucy_mamiie Pls log a call on 0860037566  Date: 2019-11-29 12:46:53  municipality: NaN hashtags: NaN , 'incorrect'
+    assert Function4.extract_municipality_hashtags(twitter_df.copy()).iloc[[4]] == 4 Tweets: '#ESKOMFREESTATE #MEDIASTATEMENT : ESKOM SUSPEN...' Date: 2019-11-29 12:17:43  municipality: NaN hashtags: '[#eskomfreestate, #mediastatement]' , 'incorrect'
+    
