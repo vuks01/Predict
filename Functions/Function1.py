8bdc661a60ffd4eabb6e76dee543ae4d8ba8398c
Nthabeleng Vilakazi <u16030266@tuks.co.za>
2020-02-19 08:41:44 +0200

    Function1.py

--- Functions/Function1.py
+++ Functions/Function1.py
@@ -7,5 +7,6 @@ def dictionary_of_metrics(items):
             
         Returns:
              returns a dictionary with the keys and their values as calculated
+             
     """
     return {'mean': round(np.mean(items),2), 'median': round(np.median(items),2), 'var': round(np.var(items,ddof=1),2), 'std': round(np.std(items,ddof=1),2), 'min':round(np.min(items),2), 'max': round(np.max(items),2)}
