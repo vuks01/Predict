a46f75ce4eebe654a5eea30b224f4f206302baf4
Nthabeleng Vilakazi <u16030266@tuks.co.za>
2020-02-19 10:24:30 +0200

    Packaged Predict locally

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     license='MIT',
     description='EDSA analyse_predict team 26',
     long_description=open('README.md').read(),
-    install_requires=['numpy, pandas'],
+    install_requires=[                      # Add project dependencies here
+        "pandas>=0.20.0" ,'numpy'                   # example: pandas version 0.20 or greater                          
+    ],
     url='https://github.com/vuks01/predict',
     author='Team26',
     author_email='vukeyasa@gmail.com'
