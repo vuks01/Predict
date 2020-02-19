d12dd76d9b7d24b9287641877763ff405565dd0a
Nthabeleng Vilakazi <u16030266@tuks.co.za>
2020-02-19 09:08:04 +0200

    test updated

--- tests/test_Function4.py
+++ tests/test_Function4.py
@@ -4,6 +4,7 @@ def test_extract_municipality_hashtags():
 
     """
     Make sure that extract_municipality_hashtags works correctly
+    
     """
     assert Function4.extract_municipality_hashtags(twitter_df.copy()).iloc[[0]] == 0 Tweets: @BongaDlulane Please send an email to mediades... Date: 2019-11-29 12:50:54 municipality: NaN hashtags: NaN , 'incorrect'
     assert Function4.extract_municipality_hashtags(twitter_df.copy()).iloc[[1]] == 1 Tweets: @saucy_mamiie Pls log a call on 0860037566  Date: 2019-11-29 12:46:53  municipality: NaN hashtags: NaN , 'incorrect'
